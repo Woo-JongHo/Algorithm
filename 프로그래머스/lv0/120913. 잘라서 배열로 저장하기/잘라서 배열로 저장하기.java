31865fc17901f934479cc07882f705b16fa0e655
Woo-JongHo <117367145+Woo-JongHo@users.noreply.github.com>
2023-04-12 15:17:09 +0900

    [level 0] Title: 잘라서 배열로 저장하기, Time: 0.04 ms, Memory: 76.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120913. 잘라서 배열로 저장하기/잘라서 배열로 저장하기.java
@@ -0,0 +1,24 @@
+class Solution {
+    public String[] solution(String my_str, int n) {
+      //01  answer의 넣을 배열길이설정
+        int len = my_str.length() / n;
+        
+        //02 배열길이가 안나누어 떨어지면 + 1로 설정
+        if (my_str.length() % n != 0) {
+            len++;
+        }
+        String[] answer = new String[len];
+        int j = 0;
+        
+        //02, 배열 설정값에 따라 케이스를 나눠준다.
+        for (int i = 0; i < len; i++) {
+            if (j + n > my_str.length()) {
+                answer[i] = my_str.substring(j);
+            } else {
+                answer[i] = my_str.substring(j, j + n);
+            }
+            j = j + n;
+        }
+        return answer;
+    }
+}
