09115ec5bb55e0510939bb94104e9bc8f1c3e4e4
Woo-JongHo <117367145+Woo-JongHo@users.noreply.github.com>
2023-04-12 21:18:27 +0900

    [level 0] Title: 2차원으로 만들기, Time: 0.03 ms, Memory: 79.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120842. 2차원으로 만들기/2차원으로 만들기.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int[][] solution(int[] num_list, int n) {
+        
+        //01. 1차,2차배열의 크기를 정해준다
+        int len = num_list.length / n;
+
+        int[][] answer = new int[len][n];
+        int [] arr = new int[n];
+        
+        //02. 1차배열은 len만큼돌고, 2차배열은 n만큼 돈다.
+        //03. num_list까지 돌기위해 k를 사용한다. 
+        int k = 0;
+        for(int i = 0; i < len;i++){
+            for(int j = 0 ; j < n ; j++)
+            {
+                answer[i][j] = num_list[k];
+                k++;
+            }
+        }
+            
+            
+        return answer;
+    }
+}
