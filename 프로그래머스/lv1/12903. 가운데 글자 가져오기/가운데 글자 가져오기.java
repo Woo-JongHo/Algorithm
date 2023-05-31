8c67e789b712c684197fccdff996cb4729dbce71
Woo-JongHo <117367145+Woo-JongHo@users.noreply.github.com>
2023-05-31 15:13:31 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 7.31 ms, Memory: 76.9 MB -BaekjoonHub

--- 프로그래머스/lv1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.java
+++ 프로그래머스/lv1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.java
@@ -10,7 +10,7 @@ class Solution {
         if (s.length() % 2 != 0)
             answer = arr[s.length() / 2] + "";
         else
-            answer = arr[s.length() / 2 - 1] + "" + arr[s.length() / 2];
+            answer = arr[s.length() / 2 - 1] + "" + arr[s.length() / 2] + "";
         
         return answer;
     }
