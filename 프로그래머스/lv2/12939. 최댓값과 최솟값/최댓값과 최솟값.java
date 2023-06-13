4db4671d7419cb612a315ce269e195515111a060
Woo-JongHo <117367145+Woo-JongHo@users.noreply.github.com>
2023-06-13 16:09:41 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 9.57 ms, Memory: 80.5 MB -BaekjoonHub

--- 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
+++ 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
@@ -1,16 +1,12 @@
-import java.util.Arrays;
 class Solution {
     public String solution(String s) {
-          
+        
         String [] arr = s.split(" ");
-        int [] num = new int[arr.length];      
-        for(int i = 0 ; i < arr.length ; i++)
-            num[i] = Integer.parseInt(arr[i]);
         
-        Arrays.sort(num);
-        
-        
-        String answer = num[0]  +" "+ num[arr.length - 1];
+        for(int i = 0 ; i < arr.length ; i++){
+            int [] num = Integer.parseInt(arr[i]);
+        }
+        String answer = "";
         return answer;
     }
 }
