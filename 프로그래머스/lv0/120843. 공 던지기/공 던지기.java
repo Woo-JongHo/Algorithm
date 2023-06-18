b305d84e57545d8fd566302d4e2916c3a5b2800f
Woo-JongHo <117367145+Woo-JongHo@users.noreply.github.com>
2023-06-18 23:00:24 +0900

    [level 0] Title: 공 던지기, Time: 0.02 ms, Memory: 77 MB -BaekjoonHub

--- 프로그래머스/lv0/120843. 공 던지기/공 던지기.java
+++ 프로그래머스/lv0/120843. 공 던지기/공 던지기.java
@@ -1,5 +1,22 @@
 class Solution {
     public int solution(int[] numbers, int k) {
-        return numbers[2 * (k - 1) % numbers.length];
+        int answer = 0;
+        int len = numbers.length;
+        int mod = 0;
+        
+        //01 numbers가 짝수이면
+        if (len % 2 == 0) {
+            //k는 len / 2 만큼 나눈 나머지만큼 돈다
+            if ( k <= len / 2 + 1) // k가 돌지 않는다면
+                answer = numbers[0 + 2 * k];
+            else
+                mod= (len % k) - 1;
+                answer = numbers[0 + 2 * mod];
+        }  else { // numbers가 홀수이면
+
+        }
+        return answer;
+
     }
+        
 }
