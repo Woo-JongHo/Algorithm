667e5cc0f4c6d91796aa82e82764463fdd3e0dbf
Woo-JongHo <117367145+Woo-JongHo@users.noreply.github.com>
2023-08-24 23:15:21 +0900

    [level 2] Title: 올바른 괄호, Time: 5.87 ms, Memory: 52.6 MB -BaekjoonHub

--- 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.java
@@ -3,12 +3,12 @@ class Solution {
         boolean answer = true;
 
         //01 개수와 순서가 맞아야한다
-        
+
         int count_a = 0;
         int count_b = 0;
-        
+
         char [] arr = s.toCharArray();
-        
+
         for (int i = 0 ; i < arr.length; i ++){
             if(arr[i] == '(')
                 count_a = count_a + 1;
@@ -18,11 +18,11 @@ class Solution {
             if(count_b > count_a)
                 answer = false;
         }
-        
+
         if (count_a != count_b){
             answer = false;
         }
-        
+
         return answer;
     }
 }
