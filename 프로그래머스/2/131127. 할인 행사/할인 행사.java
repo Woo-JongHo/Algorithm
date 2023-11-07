050ade4ea7e2c8ea6a3b7e6825d76f6791ddc6de
Woo Jong Ho <117367145+Woo-JongHo@users.noreply.github.com>
2023-11-07 22:12:00 +0900

    [level 2] Title: 할인 행사, Time: 0.04 ms, Memory: 73.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int solution(String[] want, int[] number, String[] discount) {
+    	
+        int answer = 0;     
+        
+        for(int i=0; i < discount.length - 9; i++) {
+        	int cntday = 0;
+        	for(int j = 0; j < want.length; j++) {
+        		int cnt = 0;
+        		for(int k = i; k < i + 10; k++) {
+        			if(discount[k].equals(want[j])) cnt++;       			
+        		}
+        		if(cnt<number[j]) break;
+        		else 
+                    cntday = cntday + 1;
+        	}
+        	if(cntday == want.length) 
+                answer = answer + 1;
+        }
+        
+        return answer;
+    }
+}
