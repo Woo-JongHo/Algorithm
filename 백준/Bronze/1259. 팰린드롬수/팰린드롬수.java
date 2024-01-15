40d87f790ad3ab3c7b98806449177d5622285791
Woo Jong Ho <117367145+Woo-JongHo@users.noreply.github.com>
2024-01-15 15:30:01 +0900

    [Bronze I] Title: 팰린드롬수, Time: 212 ms, Memory: 17512 KB -BaekjoonHub

---
+++ 백준/Bronze/1259. 팰린드롬수/팰린드롬수.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("0")) {
+                break;
+            }
+
+            if (isPalindrome(input)) {
+                System.out.println("yes");
+            } else {
+                System.out.println("no");
+            }
+        }
+
+        scanner.close();
+    }
+
+    private static boolean isPalindrome(String str) {
+        int length = str.length();
+        for (int i = 0; i < length / 2; i++) {
+            if (str.charAt(i) != str.charAt(length - 1 - i)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
