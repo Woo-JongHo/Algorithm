e204f6f05cb788dd183d4c4ca9ea2ce5d86b71df
Woo Jong Ho <117367145+Woo-JongHo@users.noreply.github.com>
2024-03-27 08:06:31 +0900

    [level 0] Title: 나머지 구하기, Time: 0.01 ms, Memory: 77.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120810. 나머지 구하기/나머지 구하기.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(int num1, int num2) {
+        
+        
+        //test 12345
+        int answer = num1 % num2;
+        
+        return answer;
+    }
+}
