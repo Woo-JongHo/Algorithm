d7ef04c346c30a4fc7e048e1c4cec982a372e90b
Woo Jong Ho <117367145+Woo-JongHo@users.noreply.github.com>
2024-07-03 11:39:44 +0900

    [level 1] Title: 자연수 뒤집어 배열로 만들기, Time: 0.04 ms, Memory: 72.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+class Solution {
+    public int[] solution(long n) {
+        
+        //01 long을 str열
+        String str = String.valueOf(n);
+        
+        //02 char배열로 선언
+        char [] char_arr = str.toCharArray();
+         
+        //03 차배열을 String 배열로
+        String [] str_arr = new String[char_arr.length];
+
+        int j = 0;
+        for(int i = char_arr.length - 1 ; i >= 0 ; i--){
+            str_arr[j] = String.valueOf(char_arr[i]);             
+            j++;
+        }        
+
+        //04 answer에 담기
+        int [] answer = new int[str_arr.length];
+        for(int i = 0; i < str_arr.length; i++){
+            answer[i] = Integer.parseInt(str_arr[i]);
+        }
+        
+        return answer;
+    }
+}
