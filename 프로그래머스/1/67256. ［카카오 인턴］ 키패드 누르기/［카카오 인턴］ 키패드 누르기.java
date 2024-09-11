dd99a9dc9e856796b38d504828b25f15d7009154
Woo Jong Ho <117367145+Woo-JongHo@users.noreply.github.com>
2024-09-12 01:18:54 +0900

    [level 1] Title: [카카오 인턴] 키패드 누르기, Time: 0.25 ms, Memory: 73.1 MB -BaekjoonHub

--- 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.java
+++ 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.java
@@ -1,6 +1,7 @@
 class Solution {
     public String solution(int[] numbers, String hand) {
         StringBuilder sb = new StringBuilder();
+        
         //처음 세팅
         int left = 10;
         int right = 12;
