245319a88b92c1a2a59008131f76bc30736592da
Woo Jong Ho <117367145+Woo-JongHo@users.noreply.github.com>
2025-02-17 13:08:26 +0900

    [level 1] Title: 평균 구하기, Time: 0.05 ms, Memory: 88.8 MB -BaekjoonHub

--- 프로그래머스/1/12944. 평균 구하기/평균 구하기.java
+++ 프로그래머스/1/12944. 평균 구하기/평균 구하기.java
@@ -1,6 +1,7 @@
 class Solution {
     public double solution(int[] arr) {
         double answer = 0;
+        //
         
         for(int i = 0 ; i < arr.length; i++)
             answer = answer + arr[i];
